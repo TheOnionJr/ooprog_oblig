69fd7ddb666f9aa14072e77b08e155b5d1086b62
Erlenein <erlenein@stud.ntnu.no>
2017-03-20 15:40:54 +0100

    Const.h header fil med alle conster.

---
+++ Headers/Const.h
@@ -0,0 +1,11 @@
+#pragma once
+
+//Const.h - Header fil med alle const-er.
+
+int const NVNLEN = 40;					//Navnlengde.
+int const STRLEN = 80;					//Stringlengde.
+int const NASJKORTLEN = 3;				//Max lengde, nasjonsforkortelse.
+int const MAXNASJONER = 200;			//Max antall nasjoner.
+int const MAXOVELSER = 50;				//Max antall ovelser.
+int const MINDELTAGERE = 5;				//Min antall deltagere.
+int const MAXDELTAGERE = 100;			//Max antall deltagere.
