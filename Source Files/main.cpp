7b341b94d2dbd96123d7ff971b94ce237ef454a9
phenipa <sondrgra@stud.ntnu.no>
2017-03-20 18:25:00 +0100

    redigert main

--- Source Files/main.cpp
+++ Source Files/main.cpp
@@ -6,5 +6,24 @@
 using namespace std;
 
 int main() {
+	char kommando;
+
+	skrivMeny();                        //  Skriver brukermeny.
+
+	kommando = lesKommando();           //  Får inn brukers ønske.
+										//  Looper til 'x'/'X' tastes:
+	while (kommando != 'X') {
+		switch (kommando) {
+		case 'N':	break;
+		case 'D':	break;
+		case 'G':	break;
+		case 'O':	break;
+		case 'M':	break;
+		case 'P':	break;
+		case 'X':	break;
+		default:   skrivMeny();        break;
+		}
+		kommando = lesKommando();                  //  Får inn brukers ønske.
+	}
 	return 0;
 }
