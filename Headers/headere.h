2ead5092a20ba9514b48190a62c521c1f6c8e547
Erlenein <erlenein@stud.ntnu.no>
2017-03-20 19:23:43 +0100

    headers.h oppdatert med GlobaleFunksjoner, cstring og fstream.

--- Headers/headere.h
+++ Headers/headere.h
@@ -1,6 +1,9 @@
 #pragma once
 
-#include "Const.h"			//Nødvendige const'er
-#include "ListTool2B.h"		//Definisjon av klasser
-#include <cstring>			//strcpy_s, strlen, strcmp
-#include <iostream>			//cin, cout
+#include "Const.h"					//Nødvendige const'er
+#include "ListTool2B.h"				//Definisjon av klasser
+#include "GlobaleFunksjoner.h"		//Deklarasjon av alle globale funksjoner.
+#include <cstring>					//strcpy_s, strlen, strcmp
+#include <iostream>					//cin, cout
+#include <cctype>					//toupper
+#include <fstream>					//ifstream, ofstream
