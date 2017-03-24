6caf7f0a6e76b8d812046b322c22ddd564006e06
Erlenein <erlenein@stud.ntnu.no>
2017-03-24 14:02:02 +0100

    La til Enumer.h

--- Headers/headere.h
+++ Headers/headere.h
@@ -3,8 +3,13 @@
 #include "Const.h"					//Nødvendige const'er
 #include "ListTool2B.h"				//Definisjon av klasser
 #include "GlobaleFunksjoner.h"		//Deklarasjon av alle globale funksjoner.
+<<<<<<< HEAD
 #include "Nasjon.h"
 #include "Enumer.h"					//Alle enumer.
+=======
+#include "Nasjon.h"					//Nasjon klasse header
+#include "Deltager.h"				//Deltager klasse header
+>>>>>>> 9d8467242890919d87f2cf9435130beb2212fe49
 #include <cstring>					//strcpy_s, strlen, strcmp
 #include <iostream>					//cin, cout
 #include <cctype>					//toupper
