4bc7b6647b3e6351f211c663af70c50fad7b29ce
Erlenein <erlenein@stud.ntnu.no>
2017-03-28 14:33:01 +0200

    Fikk programmet til å kompilere, ikke optimal løsning

--- Headers/main.h
+++ Headers/main.h
@@ -1,7 +1,3 @@
 #pragma once
 using namespace std;
 #include "../Headers/headere.h"
-
-List* nasjoner;						// 'Nasjoner' liste,  sortert
-List* grener;						//'Grener' liste, sortert. 
-char kommando;
