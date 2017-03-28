0e67ff2684503ea7a367322ea922585427924eb6
Erlenein <erlenein@stud.ntnu.no>
2017-03-29 01:19:32 +0200

    Initiell opplastning av Grener.cpp

---
+++ Source Files/Grener.cpp
@@ -0,0 +1,62 @@
+#include "../Headers/enumer.h"
+#include "../Headers/Const.h"
+#include "../Headers/GlobaleFunksjoner.h"
+#include "../Headers/Grener.h"
+#include <iostream>
+using namespace std;
+
+void Grener::grenerMeny() {
+	char kommando;
+	grenMeny();
+	kommando = lesKommando();		//Leser kommando.
+	while (kommando != 'X') {
+		switch (kommando) {
+		case 'N':				//Oppretter ny gren.
+			nyGren();
+			break;
+		case 'E':				//Endrer navnet på en gren.
+			endreGren();
+			break;
+		case 'A':
+			skrivHovedGren();
+			break;
+		case 'S': break;
+		default:
+			nasjonerMeny();	break;
+		}
+		grenerMeny();
+		kommando = lesKommando(); //Bruker skriver her kommando for gren-meny
+	}
+	
+
+void Grener::nyGren() {
+	char tempNvn[NVNLEN];
+
+	les("Grenens navn", tempNvn, NVNLEN);	//Leser inn navnet til grenen.
+	if (!grener->inList(tempNvn))			//Sjekker om finnes allerede.
+		grener->add(new Grener(tempNvn));	//Legger til grenen.
+	else									//Feilmelding.
+		cout << "En gren med det navnet finnes allerede!";
+}
+
+void Grener::endreGren() {
+	char grenNvn[NVNLEN];	//Temp. for sammenligning.
+	Gren* hjelpeGren;		//Hjelpepeker.
+							//Spør om navnet på gren som skal endres.
+	les("Navnet på grenen du vil endre", grenNvn, NVNLEN);
+	//Hvis grenen finnes...
+	if (grener->inList(grenNvn)) {
+		hjelpeGren = (Gren*)grener->remove(grenNvn);
+		hjelpeGren->endre();	//Fjerner, endrer, og adder
+		grener->add(hjelpeGren);//grenen i lista.
+	}
+	else {				//Feilmeld.
+		cout << "\nFant ikke en gren med det navnet!\n";
+	}
+}
+
+void Grener::skrivHovedGren() {
+	grener->displayList();
+}
+
+void Grener::skrivHovedGren() {}
