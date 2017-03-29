c2af3f88d95d4959799930107f01cda5972267d2
Erlenein <erlenein@stud.ntnu.no>
2017-03-29 05:09:30 +0200

    Plottet inn datamedlemmer i medaljer, poeng og statistikk.h

--- Headers/Poeng.h
+++ Headers/Poeng.h
@@ -1,8 +1,8 @@
 #if !defined(Poeng_h)
 #define Poeng_h
-class Poeng {
+class Poeng : NumElement {
 private:
-	
+	int poeng;			//Antall poeng en nasjon har.
 public:
 	Poeng() {
 		
--- Headers/Statistikk.h
+++ Headers/Statistikk.h
@@ -1,4 +1,11 @@
 #if !defined(Statistikk_h)
 #define Statistikk_h
 
+class Statistikk {
+	private:
+		char* nasjonsforkortelse[200];
+	public:
+		Statistikk();
+};
+
 #endif
