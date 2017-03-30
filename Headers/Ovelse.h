778709f5f57e355f4e4a37247f34f1e4e8eb043d
SanderLB <sanderlb@stud.ntnu.no>
2017-03-30 19:16:54 +0200

    Laget ovelse filer + litt

--- Headers/Ovelse.h
+++ Headers/Ovelse.h
@@ -1,3 +1,19 @@
 #if !defined(Ovelse_h)
 #define Ovelse_h
 #endif
+#include "ListTool2B.h"
+
+class Ovelse : public TextElement {
+	private:
+		char* fulltNavn;
+		char tidspunkt[8];
+		char dato[6];
+		int antDeltagere;
+		// insert deltagere liste
+		// insert resultater liste
+
+	public:
+		Ovelse();
+		Ovelse(char* id);
+		~Ovelse();
+}
