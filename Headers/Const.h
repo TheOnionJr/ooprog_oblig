866a87e062dc69104306b31e280d1870b21850da
Erlenein <erlenein@stud.ntnu.no>
2017-04-06 23:29:10 +0200

    Endret verdiene for medaljer, fordi visual studio har røyka nå og trodde det var binært

--- Headers/Const.h
+++ Headers/Const.h
@@ -18,5 +18,5 @@ int const FILLEN = 12;					//Lengden på filnavn.
 int const PSKALALEN = 10;				//Poengskala lengde. (hvor mange som skal få poeng)
 int const POENGSKALA[PSKALALEN] = { 7, 5, 4, 3, 2, 1, 1, 1, 1, 1 };	//Poengskala array.
 int const ANTMED = 3;					//Antall medaljer. 
-int const MEDVERD[ANTMED] = { 010000, 000100, 000001};				//Verdien for medaljer.
+int const MEDVERD[ANTMED] = { 10000, 100, 1};				//Verdien for medaljer.
 #endif
